d714e0ed63e10c78fdcc6b6c748e0a178f893632
alinafe82 <alinafe.matenda@gmail.com>
2017-11-07 14:50:30 -0800

    adding files

--- students/alinafe/session05/countEvens.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-nums =[1,2,3,4,5,6,56565,4,5656]
-def count_evens(nums):
-   return len([x for x in nums if not x%2])
-
-print(count_evens(nums))
--- students/alinafe/session05/foodPrefs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-food_prefs = {"name": "Chris",
-              "city": "Seattle",
-              "cake": "chocolate",
-              "fruit": "mango",
-              "salad": "greek",
-              "pasta": "lasagna"}
