f7bcd4467c187af86620a412c929b95fe18c27d9
Alinafe Matenda <alinafe.matenda@gmail.com>
2018-01-25 06:49:43 -0800

    Context manager excercise

--- students/alinafe/winter-session03/context_manager.py
+++ students/alinafe/winter-session03/context_manager.py
@@ -1,5 +1,6 @@
 # Demo of a contextmanager
 
+
 class Context(object):
     """
     from Doug Hellmann, PyMOTW
@@ -15,6 +16,28 @@ class Context(object):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print(exc_val.args)
         print('__exit__({}, {}, {})'.format(exc_type, exc_val, exc_tb))
-        return self.handle_error
+        if exc_type == ValueError:
+            return True
+        else:
+            return False
+        #return self.handle_error
+
+"""with Context(True) as cont:
+    print("in the context")
+    raise ValueError("An Error")
+__init__(True)
+__enter__()
+in the context
+('An Error',)
+__exit__(<class 'ValueError'>, An Error, <traceback object at 0x104a88608>)
+with Context(True) as cont:
+    print("in the context")
+    raise AttributeError("An Error")
+__init__(True)
+__enter__()
+in the context
+('An Error',)
+__exit__(<class 'AttributeError'>, An Error, <traceback object at 0x104a88848>)
+"""
+
